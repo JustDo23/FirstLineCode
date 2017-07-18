06ae8709eb4b93ffab34193b8fe07049ba9ba2bf
JustDo23 <757307903@qq.com>
2017-07-18 22:37:51 +0800

    在几种广播中去添加弹窗

--- app/src/main/java/com/just/first/chapter05/LocalBroadcastReceiver.java
+++ app/src/main/java/com/just/first/chapter05/LocalBroadcastReceiver.java
@@ -1,7 +1,9 @@
 package com.just.first.chapter05;
 
+import android.app.AlertDialog;
 import android.content.BroadcastReceiver;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 
 import com.just.first.utils.ToastUtil;
@@ -17,6 +19,27 @@ public class LocalBroadcastReceiver extends BroadcastReceiver {
   @Override
   public void onReceive(Context context, Intent intent) {
     ToastUtil.showShortToast(context, "Local Broadcast Receiver.");
+    showDialog(context);
+  }
+
+  /**
+   * 本地广播中无法弹出 Dialog
+   *
+   * @param context 上下文
+   */
+  private void showDialog(final Context context) {
+    AlertDialog.Builder builder = new AlertDialog.Builder(context);
+    builder.setTitle("本地广播")
+        .setMessage("在本地广播中不能显示对话框")
+        .setCancelable(false)
+        .setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+
+          @Override
+          public void onClick(DialogInterface dialog, int which) {
+            ToastUtil.showShortToast(context, "Click Ok.");
+          }
+        });
+    builder.show();
   }
 
 }
--- app/src/main/java/com/just/first/chapter05/NetworkChangeReceive.java
+++ app/src/main/java/com/just/first/chapter05/NetworkChangeReceive.java
@@ -1,7 +1,9 @@
 package com.just.first.chapter05;
 
+import android.app.AlertDialog;
 import android.content.BroadcastReceiver;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
@@ -26,6 +28,28 @@ public class NetworkChangeReceive extends BroadcastReceiver {
     } else {
       ToastUtil.showShortToast(context, "Network is unavailable.");
     }
+    showDialog(context);
+  }
+
+
+  /**
+   * 动态广播中可以弹出 Dialog
+   *
+   * @param context 上下文
+   */
+  private void showDialog(final Context context) {
+    AlertDialog.Builder builder = new AlertDialog.Builder(context);
+    builder.setTitle("动态广播")
+        .setMessage("在动态广播中可以显示对话框")
+        .setCancelable(false)
+        .setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+
+          @Override
+          public void onClick(DialogInterface dialog, int which) {
+            ToastUtil.showShortToast(context, "Click Ok.");
+          }
+        });
+    builder.show();
   }
 
 }
