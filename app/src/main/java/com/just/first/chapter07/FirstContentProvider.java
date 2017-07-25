6069d5595490e4e5c36cb93490560c62e42d674b
JustDo23 <757307903@qq.com>
2017-07-25 23:34:55 +0800

    内容提供者一点点

--- app/src/main/java/com/just/first/chapter07/FirstContentProvider.java
+++ app/src/main/java/com/just/first/chapter07/FirstContentProvider.java
@@ -2,9 +2,12 @@ package com.just.first.chapter07;
 
 import android.content.ContentProvider;
 import android.content.ContentValues;
+import android.content.UriMatcher;
 import android.database.Cursor;
 import android.net.Uri;
 import android.support.annotation.Nullable;
+
+import com.just.first.utils.LogUtils;
 
 /**
  * 7.4.1 自定义内容提供器
@@ -13,6 +16,22 @@ import android.support.annotation.Nullable;
  * @since 2017年07月24日
  */
 public class FirstContentProvider extends ContentProvider {
+
+  public static final int TABLE_1_DIR = 0;// 自定义码
+  public static final int TABLE_1_ITEM = 1;
+  public static final int TABLE_2_DIR = 2;
+  public static final int TABLE_2_ITEM = 3;
+
+  public static UriMatcher uriMatcher;// 用于匹配的对象
+  public static final String PACKAGE_NAME = "com.just.first";// 主包名
+
+  static {// 静态代码块
+    uriMatcher = new UriMatcher(UriMatcher.NO_MATCH);// 用于匹配的对象
+    uriMatcher.addURI(PACKAGE_NAME, "table1", TABLE_1_DIR);// 添加路径
+    uriMatcher.addURI(PACKAGE_NAME, "table1/#", TABLE_1_ITEM);// 可以使用通配符
+    uriMatcher.addURI(PACKAGE_NAME, "table2", TABLE_2_DIR);
+    uriMatcher.addURI(PACKAGE_NAME, "table2/#", TABLE_2_ITEM);
+  }
 
   /**
    * 初始化内容提供器。完成数据库的创建和升级操作。[只有当存在 ContentResolver 尝试访问时才会初始化]
@@ -37,6 +56,20 @@ public class FirstContentProvider extends ContentProvider {
   @Nullable
   @Override
   public Cursor query(Uri uri, String[] projection, String selection, String[] selectionArgs, String sortOrder) {
+    switch (uriMatcher.match(uri)) {// 进行匹配并返回相应的自定义码
+      case TABLE_1_DIR:
+        LogUtils.e("查询 table1 表中的所有数据");
+        break;
+      case TABLE_1_ITEM:
+        LogUtils.e("查询 table1 表中的单条数据");
+        break;
+      case TABLE_2_DIR:
+        LogUtils.e("查询 table2 表中的所有数据");
+        break;
+      case TABLE_2_ITEM:
+        LogUtils.e("查询 table2 表中的单条数据");
+        break;
+    }
     return null;
   }
 
@@ -89,6 +122,16 @@ public class FirstContentProvider extends ContentProvider {
   @Nullable
   @Override
   public String getType(Uri uri) {
+    switch (uriMatcher.match(uri)) {// 进行匹配并返回相应的自定义码
+      case TABLE_1_DIR:
+        return "vnd.android.cursor.dir/vnd.com.just.first.table1";
+      case TABLE_1_ITEM:
+        return "vnd.android.cursor.item/vnd.com.just.first.table1";
+      case TABLE_2_DIR:
+        return "vnd.android.cursor.dir/vnd.com.just.first.table2";
+      case TABLE_2_ITEM:
+        return "vnd.android.cursor.item/vnd.com.just.first.table2";
+    }
     return null;
   }
 
